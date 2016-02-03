653290b091c4ad02fa66402f32f90c50ed48c5af
Velin Branimirov Vangelov <velin.br.vangelov@gmail.com>
2016-02-03 17:58:42 +0200

    jquery implementation

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+/*
 document.addEventListener("DOMContentLoaded", function(event) {
+*/
+$(document).ready(function() {
+
 	//template function
+	/*
 	var addSongsToList = function (songList) {
 		var songListEl = document.querySelector("#song-list");
 		for(song in songList) {
@@ -12,6 +17,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			songListEl.appendChild(listItemEl);
 		}
 	}
+	*/
+	var addSongsToList = function (songList) {
+		var songListEl = $("#song-list");
+		$.each(songList, function(key, value){
+			var el = $("<li/>")
+				.html(value.name)
+				.attr("id",key)
+				.addClass("list-group-item")
+				.click(onItemClick)
+				.appendTo(songListEl);
+
+			//alternatively
+			/*
+				$("<li/>", {
+					id: key,
+					text: value.name,
+					class: "list-group-item"
+				})
+				.click( onItemClick )
+				.appendTo(songListEl);
+			*/
+		})
+	}
+
 	var onItemClick = function (clickEvent) {
 		var song = SONGLIST[clickEvent.target.id];
 		playSong(song);
@@ -19,6 +48,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		displayAsActive(clickEvent.target);
 	}
 
+	/*
 	var displayAsActive = function (newActive) {
 		var children = document.querySelector("#song-list").children;
 		Array.prototype.forEach.call(children,function(item){
@@ -26,7 +56,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		});
 		newActive.classList.add("active");
 	}
+	*/
+	var displayAsActive = function (newActive) {
+		$(newActive)
+			.addClass("active")
+			.siblings()
+			.removeClass("active");
+		//alt
+		/*
 
+		$("#song-list")
+			.children()
+			.removeClass("active");
+		$(newActive).addClass("active");
+
+		*/
+	}
+
+	/*
 	var playSong = function (song) {
 		PLAYER = document.querySelector("#player");
 		var track = document.createElement('source');
@@ -36,14 +83,45 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		PLAYER.load();//reloads the player
 		PLAYER.play();
 	}
+	*/
+	var playSong = function (song) {
+		var player = $("#player")[0];
+		var songSource = $("<source/>", {
+			src: song.src
+		});
+		$(player)
+			.empty()
+			.append(songSource);
 
+		player.load();
+		player.play();
+		//alt
+		/*
+		var player = $("#player")[0];
+		$(player)
+			.empty()
+			.append('<source src=" + song.src + " />');
+		player.load();
+		player.play();
+		 */
+	}
+
+	/*
 	var displaySongInfo = function (song) {
 		document.querySelector("#song-name").innerHTML = song.name;
 		document.querySelector("#song-artist").innerHTML = song.artist;
 		document.querySelector("#song-url").innerHTML = song.src;
 		document.querySelector("#song-description").innerHTML = song.info;
 	}
+	*/
+	var displaySongInfo = function (song) {
+		$("#song-name").html(song.name);
+		$("#song-artist").html(song.artist);
+		$("#song-url").html(song.src);
+		$("#song-description").html(song.info);
+	}
 
+	/*
 	var loadSongsRequest = new XMLHttpRequest();
 
 	//we attach our response logic
@@ -61,4 +139,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	loadSongsRequest.open('GET', 'songs.json', true);
 	//send the request
 	loadSongsRequest.send();
+	*/
+
+	$.ajax({
+		dataType: "json",
+		url: "songs.json",
+		success: function (data) {
+			SONGLIST = data;
+			addSongsToList(SONGLIST);
+		}
+	});
+	//same as
+	/*
+	$.getJSON('song.json', function () { .. });
+	 */
 });
